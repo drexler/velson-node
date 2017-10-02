831e9e0637463ef170c42598f3642d1e7a0a67e1
drexler <andrew.quartey@gmail.com>
2017-10-02 07:30:17 -0400

    implement command line options & velson api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 
+import * as colors from 'colors';
 import * as program from 'commander';
+import {VelsonEngine} from './engine';
 
 program
   .version('0.1.0')
-  .option('-t, --template', 'Path to the Velocity template mapping file')
-  .option('-r, --request', 'Path to request JSON file.')
+  .option('-t, --template [value]', 'Path to the Velocity template mapping file. (required)')
+  .option('-r, --request [value]', 'Path to request JSON file. (required)')
   .parse(process.argv);
+
+if (program.template && program.request) {
+  const engine = new VelsonEngine();
+  try {
+    engine.initialize(program.template, program.request);
+    const transformedOutput = engine.transform();
+    console.log(colors.green(JSON.stringify(transformedOutput, null, 2)));
+  } catch (error) {
+    console.log(error.message);
+  }
+} else {
+  program.outputHelp((txt) => {
+    return colors.yellow(txt);
+  });
+}
